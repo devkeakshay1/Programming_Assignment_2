bd6e821d6822fd1c7a25a1f4ca00e937477b104d
devkeakshay1 <64247610+devkeakshay1@users.noreply.github.com>
2022-11-27 17:41:17 +0530

    Add files via upload

---
+++ Question_1.py
@@ -0,0 +1,17 @@
+
+### 1. Write a Python program to convert kilometers to miles?
+##### Sol.
+
+km = float(input("The value in Km is :"))
+
+#The formula for conversion of km into miles is miles=km/1.60934
+
+miles = km/1.60934
+
+print("For {}km equivalent distance in miles = {}".format(km, miles))
+
+
+#############################################
+#########ANSWER##################
+#The value in Km is :6
+#For 6.0km equivalent distance in miles = 3.7282364198988405
