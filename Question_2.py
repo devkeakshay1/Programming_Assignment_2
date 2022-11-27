233f08313e6c86bf9cc036dc2c756f8c4dbae866
devkeakshay1 <64247610+devkeakshay1@users.noreply.github.com>
2022-11-27 17:50:37 +0530

    Add files via upload

---
+++ Question_2.py
@@ -0,0 +1,19 @@
+### 2.Write a Python program to convert Celsius to Fahrenheit?
+##### Sol.
+
+Celsius=float(input("The temperature in Celsius is : "))
+
+#The conversion formula for Celsius to Fahrenheit is f = (9*c/5)+32
+
+Fahrenheit=(9*Celsius/5)+32
+
+print("The Celsius is ",Celsius," and its canverted Fahrenheit is ",Fahrenheit)
+print("Celsius=",Celsius)
+print("Fahrenheit",Fahrenheit)
+
+##################################
+##########ANSWER#################
+#The temperature in Celsius is : 35
+#The Celsius is  35.0  and its canverted Fahrenheit is  95.0
+#Celsius= 35.0
+#Fahrenheit 95.0
